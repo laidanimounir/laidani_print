d7940e2d2f47e69035ba9ae470586ea086f92002
laidanimounir <laidanimounir606@gmail.com>
2026-05-22 16:35:15 +0100

    config: setup named routes with role protection

---
+++ lib/config/routes.dart
@@ -0,0 +1,16 @@
+class AppRoutes {
+  // TODO: add deep link support for order tracking
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String customerUpload = '/upload';
+  static const String customerConfirm = '/confirm';
+  static const String customerTrack = '/track';
+  static const String workerDashboard = '/worker';
+  static const String orderDetail = '/worker/order';
+  static const String managerDashboard = '/manager';
+  static const String managerWorkers = '/manager/workers';
+  static const String managerReports = '/manager/reports';
+  static const String managerCustomers = '/manager/customers';
+  static const String managerCustomerDetail = '/manager/customers/detail';
+  static const String managerSettings = '/manager/settings';
+}
